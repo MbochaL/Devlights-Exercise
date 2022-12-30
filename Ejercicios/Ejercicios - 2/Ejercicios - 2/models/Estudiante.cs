b5ad9a0c74532698991c984a2c846513f8c554cd
Martin Bosch <92443557+MbochaL@users.noreply.github.com>
2022-12-30 14:55:33 -0300

    Update Estudiante.cs

--- Ejercicios/Ejercicios - 2/Ejercicios - 2/models/Estudiante.cs
+++ Ejercicios/Ejercicios - 2/Ejercicios - 2/models/Estudiante.cs
@@ -12,19 +12,6 @@ using System.Threading.Tasks;
 
 namespace Exercise15P2.models
 
-//    15.Crear una clase Libro, que contenga Nombre, Autor, Cantidad de Páginas, y
-//    una property llamada WasRead, que diga si el libro fue leído o no(boolean).
-//    Crear una clase Estudiante, que hereda de Persona(la clase de los puntos
-//    anteriores). El estudiante debe contener una lista de Libros, los cuales puede
-//    ser que los haya leído o no. (Se pueden crear dos listas diferentes si se
-//    complica mucho.Una lista con todos los libros, leídos o no, y otra lista sólo
-//    con los libros leídos) Debe haber un método que muestre la lista de libros
-//    leídos, y además otro método que permita calcular la suma de la cantidad de
-//    páginas leídas totales, de todos los libros leídos.Y un método que permita
-//    saber cuántos libros quedan sin leer. Otro método que permite agregar o
-//    quitar libros de la lista de libros (dos métodos diferentes).
-
-
 {
     class Estudiante : Persona
     {
